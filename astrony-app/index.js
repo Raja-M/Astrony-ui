57480ec3670e7cb617789e7f352bf12cd1ebe4d4
Raja M <srinivasmrv@gmail.com>
2026-02-03 15:07:42 -0600

    Astrony-app

---
+++ astrony-app/index.js
@@ -0,0 +1,29 @@
+var express = require("express"); 
+var https = require("https");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+ 
+app.use(express.static('public'));
+ 
+//make way for some custom css, js and images
+app.use('/plugins', express.static(__dirname + '/public/plugins'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/scss', express.static(__dirname + '/public/scss'));
+
+const sslServer = https.createServer(
+    { 
+        key: fs.readFileSync(path.join(__dirname, 'certs', 'astrony.com.key')),
+        cert: fs.readFileSync(path.join(__dirname, 'certs', 'astrony.com.crt')),
+    },app
+)
+sslServer.listen(443, () => console.log('Secure server started on port 443'));
+
+/*var server = app.listen(80, function(){
+    var port = server.address().port;
+        console.log("Server started at http://localhost:%s", port);
+});
+*/
